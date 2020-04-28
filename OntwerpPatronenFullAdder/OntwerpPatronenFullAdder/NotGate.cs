d0a02b22216ccf95cee86064851d8bf8e09d7634
Dion <djlegierse@hotmail.com>
2020-04-28 13:38:38 +0200

    Added and finished not gate

---
+++ OntwerpPatronenFullAdder/OntwerpPatronenFullAdder/NotGate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OntwerpPatronenFullAdder
+{
+    class NotGate : SimpleGate
+    {
+        private IGate inputGate = null;
+
+        private NotGate(int id) : base(id)
+        {
+
+        }
+
+        static SimpleGate CreateNotGate(int id)
+        {
+            return new NotGate(id);
+        }
+
+        public new bool GetState()
+        {
+            return !inputGate.GetState();
+        }
+
+        public new bool AddInput(IGate gate)
+        {
+            if (this.inputGate == null)
+            {
+                this.inputGate = gate;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
