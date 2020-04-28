5f9ba5e476ebfe4c9925413b16a335a8f217839b
Dion <djlegierse@hotmail.com>
2020-04-28 13:34:00 +0200

    Added and finished or gate

---
+++ OntwerpPatronenFullAdder/OntwerpPatronenFullAdder/OrGate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OntwerpPatronenFullAdder
+{
+    class OrGate : SimpleGate
+    {
+        private List<IGate> inputGates;
+
+        private OrGate(int id) : base(id)
+        {
+
+        }
+
+        static SimpleGate CreateOrGate(int id)
+        {
+            return new OrGate(id);
+        }
+
+        public new bool GetState()
+        {
+            foreach (IGate gate in this.inputGates)
+            {
+                if (gate.GetState())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public new bool AddInput(IGate gate)
+        {
+            this.inputGates.Add(gate);
+            return true;
+        }
+    }
+}
